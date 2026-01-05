e7d610e3cf849c5d5c8131bf1cf091b537c71d74
nickmahd <nickmahda@gmail.com>
2026-01-04 18:41:09 -0800

    .

--- install.sh
+++ install.sh
@@ -4,10 +4,16 @@ set -e
 
 WDIR=$PWD
 
-sudo apt install -y nvidia-driver-580
+cd ~
+
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/sbsa/cuda-keyring_1.1-1_all.deb
+sudo dpkg -i cuda-keyring_1.1-1_all.deb
+sudo apt update
+sudo apt install -y nvidia-driver-580-open libcudnn9-dev-cuda-12
+
 git clone https://github.com/lightvector/KataGo.git
 cd KataGo/cpp
 wget https://media.katagotraining.org/uploaded/networks/models/kata1/kata1-b28c512nbt-adam-s11165M-d5387M.bin.gz -o network.bin.gz
 cp $PWD/analysis.cfg .
-cmake . -DUSE_BACKEND=OPENCL
+cmake . -DUSE_BACKEND=CUDA
 make -j
