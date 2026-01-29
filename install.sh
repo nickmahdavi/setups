eab3e21d92b70b4c2e23675baedd3d836b102c15
nickmahdavi <nickmahda@gmail.com>
2026-01-28 20:01:49 -0500

    update

--- install.sh
+++ install.sh
@@ -6,14 +6,20 @@ WDIR=$PWD
 
 cd ~
 
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/$(uname -m)/cuda-keyring_1.1-1_all.deb
+arch=$(uname -m)
+
+if [ "$arch" = "aarch64" ] && [ -d /sys/firmware/acpi ]; then
+    arch="sbsa"
+fi
+
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/$arch/cuda-keyring_1.1-1_all.deb
 sudo dpkg -i cuda-keyring_1.1-1_all.deb
 sudo apt update
 sudo apt install -y libcudnn9-dev-cuda-12
 
 git clone https://github.com/lightvector/KataGo.git
 cd KataGo/cpp
-wget https://media.katagotraining.org/uploaded/networks/models/kata1/kata1-b28c512nbt-adam-s11165M-d5387M.bin.gz -O network.bin.gz
+wget https://media.katagotraining.org/uploaded/networks/models/kata1/kata1-b28c512nbt-s12192929536-d5655876072.bin.gz -O network.bin.gz
 cp $WDIR/analysis.cfg .
 cmake . -DUSE_BACKEND=CUDA
 make -j
